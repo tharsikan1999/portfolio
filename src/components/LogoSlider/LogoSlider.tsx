bc5f00014ba385891261a7d7b8278d20e782fbee
tharsikan1999 <tharsikanking@gmail.com>
2024-06-12 21:29:48 +0530

    Feat : Finish Logoslider Animations

--- src/components/LogoSlider/LogoSlider.tsx
+++ src/components/LogoSlider/LogoSlider.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import cv from "@/assets/cv/Tharsikan_software_engineer_cv.pdf";
 
 const LogoSlider = () => {
   const cvFilePath = cv;
 
+  const { ref, inView } = useInView({
+    triggerOnce: false, // Trigger animation once
+    threshold: 0.2, // Trigger animation when 20% of the element is in view
+  });
+
   return (
     <>
       {/*----- Start Cta -----*/}
-      <section className="py-10 bg-slate-900" id="LogoSlider">
+      <section ref={ref} className="py-10 bg-slate-900" id="LogoSlider">
         <div className="container">
-          <div className="grid grid-cols-12 gap-5 items-center">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{
+              opacity: inView ? 1 : 0,
+              y: inView ? 0 : 50,
+              transition: { duration: 0.6, ease: "easeOut" },
+            }}
+            className="grid grid-cols-12 gap-5 items-center"
+          >
             <div className="col-span-12 lg:col-span-8 md:col-span-7 text-center md:text-left">
               <h4 className="text-2xl md:text-3xl lg:text-4xl text-white font-semibold">
                 Unlock My Professional Journey
@@ -25,7 +40,7 @@ const LogoSlider = () => {
                 Download CV
               </a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </section>
       {/*----- End Cta -----*/}
