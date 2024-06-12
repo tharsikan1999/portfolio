6e8159b123ca7d1d384c2e01d481aa4bf43c7b39
tharsikan1999 <tharsikanking@gmail.com>
2024-06-12 21:52:20 +0530

    Feat : Finish Footer Animations

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { FooterData } from "./FooterData";
 
 const Footer = () => {
   const CurrentYear = new Date().getFullYear();
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
 
   return (
     <>
       {/*----- Start Footer -----*/}
-      <footer className="bg-slate-900 border-t border-white border-opacity-10 py-4">
+      <motion.footer
+        ref={ref}
+        className="bg-slate-900 border-t border-white border-opacity-10 py-4"
+      >
         <div className="container">
           <div className="grid grid-cols-12">
-            <div className="col-span-12 md:col-span-6 flex pb-3 md:pb-0">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={inView ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ delay: 0.2, duration: 0.6 }}
+              className="col-span-12 md:col-span-6 flex pb-3 md:pb-0"
+            >
               <div className="flex justify-center items-center md:justify-start w-full">
                 {FooterData.map((item, index) => (
                   <a
@@ -24,15 +38,20 @@ const Footer = () => {
                   </a>
                 ))}
               </div>
-            </div>
-            <div className="col-span-12 md:col-span-6 text-center md:text-right">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={inView ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ delay: 0.4, duration: 0.6 }}
+              className="col-span-12 md:col-span-6 text-center md:text-right"
+            >
               <p className="m-0 text-white text-opacity-75">
                 © {CurrentYear} Tharsikan. All rights reserved.
               </p>
-            </div>
+            </motion.div>
           </div>
         </div>
-      </footer>
+      </motion.footer>
       {/*----- End Footer -----*/}
     </>
   );
