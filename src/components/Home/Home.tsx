a65a0da7cd160dd86863be5807af4374c2ecc47d
tharsikan1999 <tharsikanking@gmail.com>
2024-06-12 21:28:15 +0530

    Feat : Finish Homepage Animations

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import home_banner from "@/assets/Image/home-banner.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Home = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.2,
+  });
+
   return (
     <>
       {/*----- Start Home -----*/}
       <section
+        ref={ref}
         className="pt-[120px] pb-[80px] lg:pt-[180px] lg:pb-[100px] bg-orange-50 relative overflow-hidden"
         id="home"
       >
         <div className="container relative z-[1]">
           <div className="grid lg:grid-cols-12 grid-cols-1 items-center">
-            <div className="lg:col-span-6">
+            <motion.div
+              initial={{ opacity: 0, y: 50, x: -50 }}
+              animate={{
+                opacity: inView ? 1 : 0,
+                y: inView ? 0 : 50,
+                x: inView ? 0 : -50,
+                transition: { duration: 0.6, ease: "easeOut" },
+              }}
+              className="lg:col-span-6"
+            >
               <div className="text-center lg:text-start mb-14 lg:mb-0">
                 <h6 className="uppercase text-sm md:text-base tracking-[2px] font-semibold mb-5 md:mb-8">
                   👋, My name is Tharsikan
@@ -35,8 +52,16 @@ const Home = () => {
                   </a>
                 </div>
               </div>
-            </div>
-            <div className="lg:col-span-6">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              animate={{
+                opacity: inView ? 1 : 0,
+                x: inView ? 0 : 50,
+                transition: { duration: 0.6, ease: "easeOut" },
+              }}
+              className="lg:col-span-6"
+            >
               <div className="home-image text-center lg:ml-0 md:ml-[60px] sm:ml-o">
                 <Image
                   src={home_banner}
@@ -45,7 +70,7 @@ const Home = () => {
                   className="rounded-3xl"
                 />
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
